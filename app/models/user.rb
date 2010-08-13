f1bd603594518d5806fd23111ad91bb96e193037
Raymond Vellener <raymond.vellener@brightin.nl>
2010-08-13 11:34:19 +0200

    User email should be downcased in al cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   def self.authenticate(email_address, password)
-    find_by_email_address_and_password(email_address, password)
+    find_by_email_address_and_password(email_address.downcase, password)
+  end
+  
+  def before_save
+      self.username.downcase!
   end
 end
