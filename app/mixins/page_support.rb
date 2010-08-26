8bce9f937153f0e94701e09719cf924c2357fe78
jvdp <jvdplas@gmail.com>
2010-08-26 11:00:32 +0200

    Mixin for supporting pages for the frontend site Shared partial for rendering menu's for the frontend site

---
+++ app/mixins/page_support.rb
@@ -0,0 +1,30 @@
+# Mixin for using the Page-model in the frontend site
+module PageSupport
+  def self.included(base)
+    base.send :before_filter, :set_page
+    base.send :helper_method, :homepage?, :active?
+  end
+  
+  private
+  
+  # before_filter to set @page
+  def set_page
+    path = params[:path] || request.path
+    path = "/#{path}" if !path.starts_with?('/')
+    @page = Page.all.detect{|p| p.url == path}
+    
+    if @page.nil?
+      path = path [0..path.rindex('/')-1]
+      @page = Page.all.detect{|p| p.url == path}
+    end
+  end
+  
+  # helpers
+  def homepage?
+    @page.homepage?
+  end
+  
+  def active?(page)
+    @page.path.include? page
+  end
+end
