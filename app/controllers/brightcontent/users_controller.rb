94de7daf8645c3579cf9b08a9438d7a101eceabb
Egbert Veenstra <egbertveenstra@gmail.com>
2011-09-15 18:09:01 +0200

    merge fixes

--- app/controllers/brightcontent/users_controller.rb
+++ app/controllers/brightcontent/users_controller.rb
@@ -1,3 +1,11 @@
 class Brightcontent::UsersController < Brightcontent::ApplicationController
   respond_to :html
+  
+  before_filter :set_form_fields, only: %w[new edit update]
+  
+  private
+  
+  def set_form_fields
+    @form_fields ||= model.column_names + %w[password password_confirmation] - %w[created_at updated_at password_digest]
+  end
 end
