a3c746b01d086a260ddfca293cb00be88ad40ee3
Egbert Veenstra <egbertveenstra@gmail.com>
2011-11-07 12:44:34 +0100

    adde railtie to load haml

--- lib/brightcontent.rb
+++ lib/brightcontent.rb
@@ -7,6 +7,7 @@ require 'paperclip'
 require "brightcontent/engine"
 require 'brightcontent/page_urls'
 require 'brightcontent/generic_field_helper'
+require 'brightcontent/railtie'
 
 module Brightcontent
 end
