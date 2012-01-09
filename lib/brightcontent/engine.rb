d66e6dfaa48266b32e026d4f79bdf7486d6d5255
Lasse Capel <lasse@brightin.nl>
2012-01-09 11:13:47 +0100

    precompile wymeditor and custom.css assets in Engine (not yet for production)

--- lib/brightcontent/engine.rb
+++ lib/brightcontent/engine.rb
@@ -1,5 +1,11 @@
 module Brightcontent
   class Engine < Rails::Engine
     # isolate_namespace Brightcontent
+    # Enabling assets precompiling under rails 3.1
+    # if Rails.version >= '3.1'
+    #     initializer :assets do |config|
+    #       Rails.application.config.assets.precompile += %w( wymeditor/wymeditor.css wymeditor/wymeditor.js brightcontent/custom.css )
+    #     end
+    #   end
   end
 end
