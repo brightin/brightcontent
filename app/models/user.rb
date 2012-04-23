33fc85c8b3d2f78ea0053934f84b9606038e4d2a
Kris van der Ven <kris@brightin.nl>
2012-04-23 14:07:22 +0200

    validates uniqueness of emailaddress for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   attr_accessible :email_address, :password, :password_confirmation  
   has_secure_password  
+  
+  validates :email_address,   
+            :presence => true,   
+            :uniqueness => true,   
+            :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+
   validates_presence_of :password, :on => :create  
 
   def self.authenticate(email_address, password)
