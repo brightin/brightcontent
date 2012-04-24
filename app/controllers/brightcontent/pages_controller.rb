6ea1b344f551c43c5051b5c658e08f8faa375c3d
Kris van der Ven <kris@brightin.nl>
2012-04-24 20:59:47 +0200

    pages params was passed as string into acts_as_list, forced it to be an integer

--- app/controllers/brightcontent/pages_controller.rb
+++ app/controllers/brightcontent/pages_controller.rb
@@ -1,14 +1,14 @@
 class Brightcontent::PagesController < Brightcontent::ApplicationController
   def create
     super
-    @page.insert_at(params[:page][:position] || 1) unless @page.new_record?
+    @page.insert_at(params[:page][:position].to_i || 1) unless @page.new_record?
   end
   
   def update
     @page = Page.find(params[:id])
     @page.remove_from_list
     @page.parent_id = params[:page][:parent_id]
-    @page.insert_at(params[:page][:position] || 1)
+    @page.insert_at(params[:page][:position].to_i || 1)
     super
   end  
   
