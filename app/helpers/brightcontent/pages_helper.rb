3d4de435a065c0c391bcb0f44c78b98c70dd2f32
Raymond Vellener <raymond@brightin.nl>
2012-05-16 23:37:18 +0200

    Solve .last? / acts_as_list issue
    * acts_as_list scopes should be a symbols OR
      strings like 'parent_id = #{parent_id}' (note the single quotes!)
    * revert old hacks

--- app/helpers/brightcontent/pages_helper.rb
+++ app/helpers/brightcontent/pages_helper.rb
@@ -20,13 +20,12 @@ module Brightcontent::PagesHelper
     result = ""
     
     page.ancestors[0..-2].reverse.each do |p|
-      name = Page.where(:parent_id => p.parent_id).order("position DESC").first == p ? "empty" : "l"
+      name = p.last? ? "empty" : "l"
       result += image_tag( "brightcontent/#{name}.gif", :class => 'page_tree')
     end
     
     filename = "t"
-    # Ideally we want to use 'if page.last?' but acts_as_list/rails3.2 has an issue with sanitizetizing a hash in acts_as_list->list.rb
-    filename = "end" if Page.where(:parent_id => page.parent_id).order("position DESC").first == page
+    filename = "end" if page.last?
     filename = "empty" if skip_last
     return result + image_tag("brightcontent/#{filename}.gif", :class => 'page_tree')
   end
