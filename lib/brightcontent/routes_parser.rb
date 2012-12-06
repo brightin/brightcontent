b5e39cb0127921db7953d67e0f10797eea8824e3
Stefan Roex <info@stefanroex.nl>
2012-12-06 21:23:12 +0100

    Ignore default controller routes

--- lib/brightcontent/routes_parser.rb
+++ lib/brightcontent/routes_parser.rb
@@ -1,17 +1,16 @@
-require 'active_support/inflector'
-
 class RoutesParser
+  IGNORED_RESOURCES = %w{pages sessions}
 
   def self.parse(routes_hash=nil)
     new(routes_hash).resources
   end
 
   def initialize(routes_hash=nil)
-    @routes_hash = routes_hash || Rails.application.routes.routes.map(&:defaults)
+    @routes_hash = routes_hash || Brightcontent::Engine.routes.routes.map(&:defaults)
   end
 
   def resources
-    controllers_paths.map { |path| path.split("brightcontent/").last.humanize }
+    controllers_paths.map { |path| path.split("brightcontent/").last } - IGNORED_RESOURCES
   end
 
   private
--- spec/lib/brightcontent/routes_parser_spec.rb
+++ spec/lib/brightcontent/routes_parser_spec.rb
@@ -11,13 +11,23 @@ describe RoutesParser do
     ]
   end
 
-  it "should extract resource from hash" do
-    RoutesParser.parse(routes_hash).should eq ["Blogs"]
+  it "extracts resource from hash" do
+    RoutesParser.parse(routes_hash).should eq ["blogs"]
   end
 
-  it "should extract multiple resources from hash" do
+  it "extracts multiple resources from hash" do
     routes_hash << {:action=>"index", :controller=>"brightcontent/articles"}
-    RoutesParser.parse(routes_hash).should eq ["Blogs", "Articles"]
+    RoutesParser.parse(routes_hash).should eq ["blogs", "articles"]
+  end
+
+  it "ignores the pages route" do
+    routes_hash << {:action=>"index", :controller=>"brightcontent/pages"}
+    RoutesParser.parse(routes_hash).should eq ["blogs"]
+  end
+
+  it "ignores the pages route" do
+    routes_hash << {:action=>"index", :controller=>"brightcontent/sessions"}
+    RoutesParser.parse(routes_hash).should eq ["blogs"]
   end
 
 end
