7674f5df054d0aa1cea7341e8b87d05ed2a10299
Stefan Roex <info@stefanroex.nl>
2012-12-07 12:13:55 +0100

    Ability to add items

--- spec/features/resources_form_spec.rb
+++ spec/features/resources_form_spec.rb
@@ -1,16 +1,35 @@
 require 'spec_helper'
 
 feature "Resources form" do
-  before do
+  background do
     valid_login
     @blog = create(:blog)
-    visit brightcontent.blog_url(@blog)
+    visit brightcontent.blogs_url
   end
 
-  it "should show the edit form" do
+  scenario "shows edit form" do
+    click_link "Edit"
     page.should have_content "Edit"
-    save_and_open_page
     page.should have_field "Name"
     page.should have_field "Body"
   end
+
+  scenario "add a item" do
+    click_link "Add"
+    fill_in "Name", with: "test-name"
+    fill_in "Body", with: "test-body"
+    click_button "Create Blog"
+    page.should have_content "test-name"
+    page.should have_content "test-body"
+  end
+
+  scenario "edit a item" do
+    click_link "Edit"
+    fill_in "Name", with: "edit-test-name"
+    fill_in "Body", with: "edit-test-body"
+    click_button "Update Blog"
+    page.should have_content "edit-test-name"
+    page.should have_content "edit-test-body"
+  end
+
 end
