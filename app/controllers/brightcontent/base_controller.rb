652a36ced37043430e6768b254078a58d1bac72c
Stefan Roex <info@stefanroex.nl>
2012-12-07 12:13:55 +0100

    Ability to add items

--- app/controllers/brightcontent/base_controller.rb
+++ app/controllers/brightcontent/base_controller.rb
@@ -9,6 +9,18 @@ module Brightcontent
       redirect_to action: :edit
     end
 
+    def create
+      create! { polymorphic_url(resource_class) }
+    end
+
+    def update
+      update! { polymorphic_url(resource_class) }
+    end
+
+    def destroy
+      destroy! { polymorphic_url(resource_class) }
+    end
+
     private
 
     def collection_name
