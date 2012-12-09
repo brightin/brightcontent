1365110755f3986c4a34bf1badcc2f2dc890ddf5
Stefan Roex <info@stefanroex.nl>
2012-12-09 11:05:32 +0100

    Schema.rb commit

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121207132810) do
+ActiveRecord::Schema.define(:version => 20121208145516) do
 
   create_table "blogs", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(:version => 20121207132810) do
     t.string   "slug"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "depth"
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.integer  "parent_id"
   end
 
   add_index "pages", ["slug"], :name => "index_pages_on_slug"
