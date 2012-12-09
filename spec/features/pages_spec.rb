cec8d3410e6c65d7c1216afe1a5b5f53b21222c3
Stefan Roex <info@stefanroex.nl>
2012-12-09 10:55:18 +0100

    Specs for pages (forgot to commit)

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
 feature "Pages" do
-  scenario "visit specific page" do
-    @page = create :page, :name => "Test titel"
-    visit "/test-titel"
+  background do
+    @page = create :page , name: "Homepage"
+  end
+
+  scenario "visit root page" do
+    visit root_path
     page.should have_content @page.name
+  end
+
+  scenario "visit specific page" do
+    @page2 = create :page, name: "Test page"
+    visit "/test-page"
+    page.should have_content @page2.name
   end
 end
