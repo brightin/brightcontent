f6ceb2b6e73bd190b310387e6f9fd7a7e58268b7
Stefan Roex <info@stefanroex.nl>
2012-12-13 19:49:57 +0100

    Refactored routes parser

--- lib/brightcontent/routes_parser.rb
+++ lib/brightcontent/routes_parser.rb
@@ -3,26 +3,39 @@ require 'active_support/inflector'
 module Brightcontent
   class RoutesParser
 
-    def self.parse(routes_hash=nil)
-      new(routes_hash).resources
+    def self.parse(routes_hash=nil, engine_resources=nil)
+      new(routes_hash, engine_resources).user_resource_classes
     end
 
-    def initialize(routes_hash=nil)
-      @routes_hash = routes_hash || Engine.routes.routes.map(&:defaults)
+    def initialize(routes_hash=nil, engine_resources=nil)
+      @routes_hash = routes_hash
+      @engine_resources = engine_resources
     end
 
-    def resources
-      resources_names.map { |name| name.classify.constantize }
+    def user_resource_classes
+      user_resource_names.map { |name| name.classify.constantize }
     end
 
     private
 
-    def resources_names
-      controllers_paths.map { |path| path.split("brightcontent/").last } - Brightcontent.engine_resources
+    def user_resource_names
+      resource_names - engine_resources
     end
 
-    def controllers_paths
-      @routes_hash.map { |route| route[:controller] if route && route[:action]=="index" }.compact
+    def resource_names
+      routes_hash.map do |route|
+        if route && route[:action] == "index"
+          route[:controller].match(/brightcontent\/(.+)/)[1]
+        end
+      end.compact
+    end
+
+    def routes_hash
+      @routes_hash ||= Engine.routes.routes.map(&:defaults)
+    end
+
+    def engine_resources
+      @engine_resources ||= Brightcontent.engine_resources
     end
 
   end
