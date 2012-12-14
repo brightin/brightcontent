ae238b0ff9874c88ff52d354b04092ffe7f657e7
Stefan Roex <info@stefanroex.nl>
2012-12-14 17:15:33 +0100

    Fix bug in helper

--- app/helpers/brightcontent/pages_helper.rb
+++ app/helpers/brightcontent/pages_helper.rb
@@ -2,7 +2,7 @@ module Brightcontent
   module PagesHelper
     def page_selector(current_page)
       Page.all.map do |page|
-        "#{page_prefix(page.depth)} #{page.name}" if page != current_page
+        ["#{page_prefix(page.depth)} #{page.name}", page.id] if page != current_page
       end.compact
     end
 
