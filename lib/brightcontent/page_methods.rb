3746a52d0d305721a6ba172d45abf7a1c1ea2d9b
Stefan Roex <info@stefanroex.nl>
2012-12-15 15:52:45 +0100

    Do not raise error when page is not found

--- lib/brightcontent/page_methods.rb
+++ lib/brightcontent/page_methods.rb
@@ -1,7 +1,7 @@
 module Brightcontent
   module PageMethods
     def current_page
-      @current_page ||= Page.find_by_slug!(request.fullpath[1..-1])
+      @current_page ||= Page.find_by_slug(request.fullpath[1..-1])
     end
   end
 end
