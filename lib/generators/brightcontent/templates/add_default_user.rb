3c7e0c7d578a36a7b9e565fe84ae54590c5c82ac
Stefan Roex <info@stefanroex.nl>
2012-12-18 19:00:05 +0100

    Fixed Default user migration

--- lib/generators/brightcontent/templates/add_default_user.rb
+++
@@ -1,9 +0,0 @@
-class AddDefaultUser < ActiveRecord::Migration
-  def up
-    Brightcontent::User.create!(:email => 'admin@example.com', :password => 'password')
-  end
-
-  def down
-    Brightcontent::User.find_by_email('admin@example.com').delete
-  end
-end
