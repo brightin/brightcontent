3b5efab7cac9df01329a107bfa7c7b78904bf118
Stefan Roex <info@stefanroex.nl>
2012-12-19 08:29:55 +0100

    Small refactorings on variouse places

--- app/controllers/brightcontent/base_controller.rb
+++ app/controllers/brightcontent/base_controller.rb
@@ -3,7 +3,6 @@ require_dependency "brightcontent/application_controller"
 module Brightcontent
   class BaseController < ApplicationController
     inherit_resources
-    helper_method :list_fields, :form_fields
 
     include DefaultActions
     include Pagination
@@ -13,10 +12,12 @@ module Brightcontent
     def list_fields
       form_fields
     end
+    helper_method :list_fields
 
     def form_fields
       default_fields
     end
+    helper_method :form_fields
 
     def default_fields
       resource_class.column_names - %w{id created_at updated_at password_digest}
