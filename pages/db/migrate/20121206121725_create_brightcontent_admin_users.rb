a62fb39d3140c9fdd86763db998192bc3188a77a
Stefan Roex <info@stefanroex.nl>
2012-12-22 17:07:25 +0100

    Added migration to pages

---
+++ pages/db/migrate/20121206121725_create_brightcontent_admin_users.rb
@@ -0,0 +1,12 @@
+class CreateBrightcontentAdminUsers < ActiveRecord::Migration
+  def change
+    create_table :brightcontent_admin_users do |t|
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps
+    end
+
+    Brightcontent::AdminUser.create!(:email => 'admin@example.com', :password => 'password')
+  end
+end
