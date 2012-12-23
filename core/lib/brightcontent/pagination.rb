8fe895bb499ce11d0a175530b163424095b1282c
Stefan Roex <info@stefanroex.nl>
2012-12-23 18:50:00 +0100

    Fixed update bug with pagination

--- core/lib/brightcontent/pagination.rb
+++ core/lib/brightcontent/pagination.rb
@@ -5,19 +5,23 @@ module Brightcontent
     extend ActiveSupport::Concern
 
     module ClassMethods
-      def per_page(number)
-        @per_page_count = number
+      def per_page_count
+        @per_page_count ||= 30
       end
 
-      def per_page_count
-        @per_page_count || 30
+      def per_page(number)
+        @per_page_count = number
       end
     end
 
     private
 
     def end_of_association_chain
-      super.paginate(page: params[:page], per_page: self.class.per_page_count)
+      if action_name == "index"
+        super.paginate(page: params[:page], per_page: self.class.per_page_count)
+      else
+        super
+      end
     end
 
   end
