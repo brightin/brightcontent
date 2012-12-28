8f1d448b31e253f743112ad6d3d3dead9476476b
Stefan Roex <info@stefanroex.nl>
2012-12-28 11:03:03 +0100

    Extra method for brightcontent

--- core/lib/brightcontent/engine.rb
+++ core/lib/brightcontent/engine.rb
@@ -5,5 +5,11 @@ module Brightcontent
     initializer "Brightcontent precompile hook", :group => :all do |app|
       app.config.assets.precompile += ['brightcontent/brightcontent.js', 'brightcontent/brightcontent.css']
     end
+
+    initializer "Add ActiveRecord column" do
+      ActiveSupport.on_load(:active_record) do
+        include ModelExtensions
+      end
+    end
   end
 end
