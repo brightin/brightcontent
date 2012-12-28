1cc68e00c90002578db087450b1f44deeb32f49a
Stefan Roex <info@stefanroex.nl>
2012-12-28 11:19:26 +0100

    Separate attachable module

--- attachments/spec/dummy/app/models/blog.rb
+++ attachments/spec/dummy/app/models/blog.rb
@@ -1,4 +1,4 @@
 class Blog < ActiveRecord::Base
   attr_accessible :body, :name, :featured
-  has_many :attachments, as: :attachable, dependent: :destroy, class_name: Brightcontent::Attachment
+  include Brightcontent::Attachable
 end
