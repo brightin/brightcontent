a7dc7f8da69ade50ca4920669fd0b5344818118d
Stefan Roex <info@stefanroex.nl>
2013-01-21 16:17:19 +0100

    Extracted pagecore for easy overwriting of page

--- pages/app/models/brightcontent/page.rb
+++ pages/app/models/brightcontent/page.rb
@@ -1,37 +1,5 @@
 module Brightcontent
   class Page < ActiveRecord::Base
-    attr_accessible :body, :name, :parent_id, :hidden
-    acts_as_nested_set
-    include Brightcontent::Attachable
-
-    validates_presence_of :name
-    after_save :update_slug
-
-    def self.default_scope
-      order(:lft)
-    end
-
-    def homepage?
-      lft == 1
-    end
-
-    def attachment_styles
-      Brightcontent.page_attachment_styles
-    end
-
-    def root_parent_children
-      root? ? children : ancestors.first.children
-    end
-
-    private
-
-    def update_slug
-      self.update_column(:slug, slug_name)
-    end
-
-    def slug_name
-      homepage? ? "" : self_and_ancestors.map { |p| p.name.parameterize }.join("/")
-    end
-
+    include Brightcontent::PageCore
   end
 end
---
+++ pages/app/models/brightcontent/page_core.rb
@@ -0,0 +1,43 @@
+module Brightcontent
+  module PageCore
+    extend ActiveSupport::Concern
+
+    included do
+      attr_accessible :body, :name, :parent_id, :hidden
+      acts_as_nested_set
+      include Brightcontent::Attachable
+
+      validates_presence_of :name
+      after_save :update_slug
+    end
+
+    module ClassMethods
+      def default_scope
+        order(:lft)
+      end
+    end
+
+    def homepage?
+      lft == 1
+    end
+
+    def attachment_styles
+      Brightcontent.page_attachment_styles
+    end
+
+    def root_parent_children
+      root? ? children : ancestors.first.children
+    end
+
+    private
+
+    def update_slug
+      self.update_column(:slug, slug_name)
+    end
+
+    def slug_name
+      homepage? ? "" : self_and_ancestors.map { |p| p.name.parameterize }.join("/")
+    end
+  end
+end
+
