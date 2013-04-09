d1fad5883c1db29f5a2511d4067245438041f4bb
Stefan Roex <info@stefanroex.nl>
2013-04-09 15:21:48 +0200

    Update javascript

--- core/app/assets/javascripts/brightcontent/main.js
+++ core/app/assets/javascripts/brightcontent/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('[data-editor]').each(function(i, elem) {
+  $('textarea[name*="content"], textarea[name*="body"], textarea[name*="editor"]').each(function(i, elem) {
     $(elem).wysihtml5({
       html: true,
       locale: "nl-NL",
@@ -13,13 +13,13 @@ $(document).ready(function(){
     });
   });
 
-  $(".insert_image").live("click", function(e){
+  $("#attachments").on("click", ".insert_image", function(e){
     e.preventDefault();
     console.log($(this).data("insertImage"));
     editor.composer.commands.exec("insertImage", { src: $(this).data("insertImage") });
   });
 
-  $(".insert_link").live("click", function(e){
+  $("#attachments").on("click", ".insert_link", function(e){
     e.preventDefault();
     console.log($(this).data());
     editor.composer.commands.exec("createLink", { href: $(this).data("insertUrl"), target: "_blank", rel: "nofollow", text: $(this).data("insertName") });
