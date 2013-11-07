3ea5d2c16d0d15e5d77774cbceadc72b0236cf57
Stefan Roex <info@stefanroex.nl>
2013-11-07 16:24:22 +0100

    fix migration again :-)

--- core/db/migrate/20121206121725_create_brightcontent_admin_users.rb
+++ core/db/migrate/20121206121725_create_brightcontent_admin_users.rb
@@ -7,6 +7,6 @@ class CreateBrightcontentAdminUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    Brightcontent::AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password => 'password')
+    Brightcontent::AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
   end
 end
