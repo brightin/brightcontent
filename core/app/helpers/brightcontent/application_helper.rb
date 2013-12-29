7ca8277a796a1d4d6ee10f250998b380370329d2
Stefan Roex <info@stefanroex.nl>
2013-12-29 15:59:27 +0100

    Show Gravatar when logged in

--- core/app/helpers/brightcontent/application_helper.rb
+++ core/app/helpers/brightcontent/application_helper.rb
@@ -1,4 +1,8 @@
 module Brightcontent
   module ApplicationHelper
+    def avatar_url(user)
+      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=30&d=mm"
+    end
   end
 end
