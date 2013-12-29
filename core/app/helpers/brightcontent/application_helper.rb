6f60298f5c3f5580c9563b64f78ba2a53acb24e2
Stefan Roex <info@stefanroex.nl>
2013-12-29 17:17:21 +0100

    Avatar over https

--- core/app/helpers/brightcontent/application_helper.rb
+++ core/app/helpers/brightcontent/application_helper.rb
@@ -2,7 +2,7 @@ module Brightcontent
   module ApplicationHelper
     def avatar_url(user)
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=30&d=mm"
+      "https://gravatar.com/avatar/#{gravatar_id}.png?s=30&d=mm"
     end
   end
 end
