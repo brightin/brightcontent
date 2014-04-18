6f191a57853dfae3aa714dc9d9b481b88837cf99
jvdp <jvdplas@gmail.com>
2014-04-18 11:14:02 +0200

    filter_fields spec + some refactoring

--- core/lib/brightcontent/view_lookup/filter_field.rb
+++ core/lib/brightcontent/view_lookup/filter_field.rb
@@ -5,7 +5,7 @@ module Brightcontent
         raise "invalid filter field: #{options[:field]}" unless field_name
         [
           options[:form].label(:"#{field_name}_eq", options[:field].humanize),
-          options[:form].select(:"#{field_name}_eq", select_options.sort, {include_blank: true}, class: "form-control input-sm")
+          options[:form].select(:"#{field_name}_eq", select_options, {include_blank: true}, class: "form-control input-sm")
         ].join(" ").html_safe
       end
 
@@ -15,18 +15,26 @@ module Brightcontent
 
       private
 
+      def field?
+        resource_class.column_names.include? options[:field].to_s
+      end
+
+      def belongs_to_association?
+        association.try :belongs_to?
+      end
+
       def field_name
-        if resource_class.column_names.include? options[:field].to_s
+        if field?
           options[:field]
-        elsif association.try :belongs_to?
+        elsif belongs_to_assocation?
           association.foreign_key
         end
       end
 
       def select_options
-        if resource_class.column_names.include? options[:field].to_s
-          raw_options
-        elsif association.try :belongs_to?
+        if field?
+          field_type == :boolean ? raw_options : raw_options.sort
+        elsif belongs_to_assocation?
           association.klass.where(association.association_primary_key => raw_options).map do |record|
             [record, record[association.association_primary_key]]
           end
