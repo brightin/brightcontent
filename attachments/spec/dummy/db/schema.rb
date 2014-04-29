72071e50b2f15128f9291895431b8d6a7bc26bd9
Tijn Schuurmans <tijn@brightin.nl>
2014-04-29 14:35:22 +0200

    fix the text the specs search for

--- attachments/spec/dummy/db/schema.rb
+++ attachments/spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140227111023) do
+ActiveRecord::Schema.define(version: 20140203140819) do
 
   create_table "blogs", force: true do |t|
     t.string   "name"
