3d6d562b09d925e8caec2f73561786beff38ca68
jvdp <jvdplas@gmail.com>
2014-06-27 20:50:47 +0200

    current_page does not throw NotFound error (added current_page! for that)

--- pages/lib/brightcontent/pages/methods.rb
+++ pages/lib/brightcontent/pages/methods.rb
@@ -2,6 +2,10 @@ module Brightcontent
   module Pages
     module Methods
       def current_page
+        @current_page ||= Page.find_by_path(request.path)
+      end
+
+      def current_page!
         @current_page ||= Page.find_by_path!(request.path)
       end
     end
