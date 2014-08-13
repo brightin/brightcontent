b2d3446734217c50277a7d7a407b785ccd62b552
Stefan Roex <info@stefanroex.nl>
2014-08-13 15:58:57 +0200

    Respond to both PUT and PATCH on attachment upload.

--- attachments/config/routes.rb
+++ attachments/config/routes.rb
@@ -1,6 +1,6 @@
 Brightcontent::Engine.routes.draw do
   get 'attachments/:type/:id' => 'attachments#show', as: :attachments
   post 'attachments/:type/:id/reposition' => 'attachments#reposition'
-  put 'attachments' => 'attachments#create', as: :new_attachment
+  match 'attachments' => 'attachments#create', as: :new_attachment, via: [:put, :patch]
   delete 'attachments/:id' => 'attachments#destroy', as: :destroy_attachment
 end
