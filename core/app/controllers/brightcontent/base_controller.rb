e8bba9874a8fed3fd98af5fe2b25cfffe111a437
jvdp <jvdplas@gmail.com>
2016-09-05 11:43:24 +0200

    don’t pretend backwards compatibility with inherited_resources API

--- core/app/controllers/brightcontent/base_controller.rb
+++ core/app/controllers/brightcontent/base_controller.rb
@@ -7,10 +7,5 @@ module Brightcontent
     include BaseControllerExt::Filtering
     include BaseControllerExt::Pagination
     include BaseControllerExt::Fields
-
-    # deprecated; included for compatibility with inherited_resources
-    def end_of_association_chain
-      base_collection
-    end
   end
 end
