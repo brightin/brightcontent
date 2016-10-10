38e5eb37df0a6f1aeece8d030dbfee61ff11e6fb
jvdp <jvdplas@gmail.com>
2016-10-10 11:17:09 +0200

    fix admin users resource

--- core/app/controllers/brightcontent/admin_users_controller.rb
+++ core/app/controllers/brightcontent/admin_users_controller.rb
@@ -3,5 +3,9 @@ require_dependency "brightcontent/application_controller"
 module Brightcontent
   class AdminUsersController < BaseController
     form_fields :email, :password, :password_confirmation
+
+    def resource_class
+      Brightcontent.user_model
+    end
   end
 end
